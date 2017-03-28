e390033783a87ff71ec4945a693bb0a262436967
marklee1219 <marklee1219@gmail.com>
2017-03-28 06:30:53 -0700

    Firebase added

--- builds/angularregistration/js/controllers/registration.js
+++ builds/angularregistration/js/controllers/registration.js
@@ -1,3 +1,26 @@
-myApp.controller('RegistrationController', ['$scope', function($scope) {
-  $scope.message = "Welcome to my App";
-}]);
+myApp.controller('RegistrationController',
+  ['$scope', '$firebase', '$firebaseAuth',
+  function($scope, $firebase, $firebaseAuth) {
+
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth();
+
+  $scope.login = function() {
+    $scope.message = "Welcome " + $scope.user.email;
+  };
+
+  $scope.register = function() {
+
+    auth.$createUserWithEmailAndPassword (
+      $scope.user.email,
+      $scope.user.password
+    ).then(function(regUser) {
+      $scope.message = "Hi " + $scope.user.firstname +
+      ", Thanks for registering";
+    }).catch(function(error) {
+      $scope.message = error.message;
+    }); //createUserWithEmailAndPassword
+
+  };//register
+
+}]);//controller
